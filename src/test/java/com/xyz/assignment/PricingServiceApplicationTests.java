024b0af6bf7acb146b02f4645b07e628980a2b01
raghugup <raghugup@adobe.com>
2020-07-05 22:54:26 +0530

    Initial Code added.,

---
+++ src/test/java/com/xyz/assignment/PricingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyz.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
